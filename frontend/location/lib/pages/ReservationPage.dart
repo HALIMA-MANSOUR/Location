43fc6b6465a0ca01f4aedf7ec72bee06ef37188b
yakine123 <yakinebenali5@gmail.com>
2025-04-28 22:21:36 +0100

    fix

--- frontend/location/lib/pages/ReservationPage.dart
+++ frontend/location/lib/pages/ReservationPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter_braintree/flutter_braintree.dart';
 
 class ReservationPage extends StatefulWidget {
   const ReservationPage({super.key});
